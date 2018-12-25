903b39838da9bac27ea963ef3dc99d0e4228ca13
Chris Dalrymple <crisbdal@gmail.com>
2018-12-24 23:49:39 -0600

    Fixed maven dependency copy issue and added basic options to gitlicense project.

--- gitlicense/src/main/java/com/sexyjackal/App.java
+++ gitlicense/src/main/java/com/sexyjackal/App.java
@@ -1,6 +1,12 @@
 package com.sexyjackal;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 /**
  * Hello world!
@@ -12,12 +18,45 @@ public class App
     {
         final Options options = new Options();
 
+        Option opt_h = Option.builder("h")
+                        .longOpt("help")
+                        .desc("Print this text.")
+                        .hasArg(false)
+                        .required(false)
+                        .build();
+
+        Option opt_b = Option.builder("b").build();
+
+        options.addOption(opt_h);
+        options.addOption(opt_b);
+
         return options;
+    }
+
+    private static void printUsage(Options options)
+    {
+        final HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("gitlicense", options);
+        return;
     }
 
     public static void main( String[] args )
     {
+        CommandLineParser parser = new DefaultParser();
         Options options = initOptions();
+        try {
+            CommandLine cmd = parser.parse(options, args);
+            if (cmd.hasOption('h') || cmd.hasOption("help"))
+            {
+                printUsage(options);
+                return;
+            }
+        } catch (ParseException e) {
+            System.err.println(e.getLocalizedMessage());
+            printUsage(options);
+            return;
+        }
+        
         System.out.println( "Hello World!" );
     }
 }

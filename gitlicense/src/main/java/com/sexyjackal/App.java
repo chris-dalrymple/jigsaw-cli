755eb81a8a66b8e43096b4ef7fd35ecbf701cc62
Chris Dalrymple <crisbdal@gmail.com>
2019-01-02 22:41:19 -0600

    Minor updates to main App class.

--- gitlicense/src/main/java/com/sexyjackal/App.java
+++ gitlicense/src/main/java/com/sexyjackal/App.java
@@ -8,10 +8,6 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     private static Options initOptions()
@@ -25,10 +21,31 @@ public class App
                         .required(false)
                         .build();
 
-        Option opt_b = Option.builder("b").build();
+        Option opt_e = Option.builder("e")
+                        .longOpt("ext")
+                        .desc("List of (comma-separated) file extensions to check.")
+                        .hasArgs()
+                        .required(false)
+                        .build();
+
+        Option opt_i = Option.builder("i")
+                        .longOpt("include")
+                        .desc("List of (comma-separated) folders to include.")
+                        .hasArg()
+                        .required(false)
+                        .build();
+
+        Option opt_x = Option.builder("x")
+                        .longOpt("exclude")
+                        .desc("List of (comma-separated) folders to exclude.")
+                        .hasArg()
+                        .required(false)
+                        .build();
 
         options.addOption(opt_h);
-        options.addOption(opt_b);
+        options.addOption(opt_e);
+        options.addOption(opt_i);
+        options.addOption(opt_x);
 
         return options;
     }
@@ -40,21 +57,39 @@ public class App
         return;
     }
 
+    private static void processCommandLine(CommandLine cmd)
+    {
+        if (cmd.hasOption("e")) {
+            System.out.println("e options: " + cmd.getOptionValue("e"));
+        } else if (cmd.hasOption("i")) {
+            System.out.println("i options: " + cmd.getOptionValue("i"));
+        } else if (cmd.hasOption("x")) {
+            System.out.println("x options: " + cmd.getOptionValue("x"));
+        } else {
+            System.out.println("Unrecognized option.");
+        }
+        return;
+    }
+
     public static void main( String[] args )
     {
         CommandLineParser parser = new DefaultParser();
         Options options = initOptions();
+        CommandLine cmd;
         try {
-            CommandLine cmd = parser.parse(options, args);
-            if (cmd.hasOption('h') || cmd.hasOption("help"))
-            {
-                printUsage(options);
-                return;
-            }
+            cmd = parser.parse(options, args);
         } catch (ParseException e) {
             System.err.println(e.getLocalizedMessage());
             printUsage(options);
             return;
+        }
+
+        if (cmd.hasOption('h'))
+        {
+            printUsage(options);
+            return;
+        } else {
+            processCommandLine(cmd);
         }
         
         System.out.println( "Hello World!" );
